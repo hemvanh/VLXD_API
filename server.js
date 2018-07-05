a07de5d61090f8a312a6a111c47db1279941acff
hiep <hiep@elitetech.vn>
2018-07-05 14:21:50 +0100

    setup gulp for deployment

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import Express from 'express'
 import GraphHttp from 'express-graphql'
+import ServeStatic from 'serve-static'
 import schema from './schema'
 
 const APP_PORT = 8888
 const app = Express()
+app.use(ServeStatic(__dirname))
 app.use(
   '/api',
   GraphHttp({
